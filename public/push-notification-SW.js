a1e94afb66f28e0494920d74bbc66119f2540d2d
Dhyey Thumar <dhyeythumar@gmail.com>
2021-12-25 17:09:53 +0530

    clean main branch for API docs

--- public/push-notification-SW.js
+++
@@ -1,9 +0,0 @@
-self.addEventListener("push", (event) => {
-    const data = event.data.json();
-    event.waitUntil(
-        self.registration.showNotification(data.title, {
-            body: data.description,
-            icon: data.icon,
-        })
-    );
-});
